03090c1c413b1f7643d743bf3e9fb7f94b628f4e
Gabriel de Oliveira Sousa <gos2@cin.ufpe.br>
2022-07-11 20:35:38 -0300

    v1.1

---
+++ src/components/header/Header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Header.css';
+
+const Header = () => {
+  return (
+    <header>
+      <div className="header--center">
+        <a href='/'>Recursos Educacionais Digitais</a>
+        <a href='/'>Sobre</a>
+      </div>
+    </header>
+  )
+}
+
+export default Header
